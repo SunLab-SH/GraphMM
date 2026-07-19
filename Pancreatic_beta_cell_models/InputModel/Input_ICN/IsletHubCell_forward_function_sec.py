016a619930d6ac1267d83b1d21149884a66fef52
ThinkJanice <97783147+ThinkJanice@users.noreply.github.com>
2026-07-19 19:15:18 +0800

    Update IsletHubCell_forward_function_sec.py

--- Pancreatic_beta_cell_models/InputModel/Input_ICN/IsletHubCell_forward_function_sec.py
+++ Pancreatic_beta_cell_models/InputModel/Input_ICN/IsletHubCell_forward_function_sec.py
@@ -8,6 +8,10 @@ from math import *
 import numpy as np
 import time
 from scipy.integrate import odeint
+from InputModel.Input_IHC.IsletHubCell_parameter import figInit
+
+def Init():
+    return figInit()
 
 
 def _f_input_ode_ICN(x, dt):
